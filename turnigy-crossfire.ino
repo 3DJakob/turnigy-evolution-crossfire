7fd32e797beacf1b0a9c07212211bd3fcf5fc093
Jakob <jakob@folkdatorn.se>
2020-02-08 18:52:21 +0100

    Create turnigy-crossfire.ino

---
+++ turnigy-crossfire.ino
@@ -0,0 +1,94 @@
+/*
+ * PPM generator originally written by David Hasko
+ * on https://code.google.com/p/generate-ppm-signal/ 
+ */
+
+//////////////////////CONFIGURATION///////////////////////////////
+#define CHANNEL_NUMBER 7  //set the number of chanels
+#define CHANNEL_DEFAULT_VALUE 1500  //set the default servo value
+#define FRAME_LENGTH 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PULSE_LENGTH 300  //set the pulse length
+#define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 10  //set PPM signal output pin on the arduino
+
+/*this array holds the servo values for the ppm signal
+ change theese values in your code (usually servo values move between 1000 and 2000)*/
+int ppm[CHANNEL_NUMBER];
+
+
+void setup(){  
+
+  //initiallize default ppm values
+  for(int i=0; i<CHANNEL_NUMBER; i++){
+      ppm[i]= CHANNEL_DEFAULT_VALUE;
+  }
+
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
+  
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+  
+  OCR1A = 100;  // compare match register, change this
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+
+}
+
+int readAndMap(int inPin) {
+  if(inPin > A4) {
+    return constrain(map(analogRead(inPin), 10, 910, 1000, 2000), 1000, 2000);
+  } else {
+   return constrain(map(analogRead(inPin), 10, 910, 2000, 1000), 1000, 2000); 
+  }
+}
+
+void loop(){
+  
+  /*
+    Here modify ppm array and set any channel to value between 1000 and 2000. 
+    Timer running in the background will take care of the rest and automatically 
+    generate PPM signal on output pin using values in ppm array
+  */
+  ppm[0] = readAndMap(A3);
+  ppm[1] = readAndMap(A2);
+  ppm[2] = readAndMap(A1);
+  ppm[3] = readAndMap(A0);
+  
+  ppm[4] = readAndMap(A4);
+  ppm[5] = readAndMap(A5); 
+  ppm[6] = readAndMap(A6); 
+}
+
+ISR(TIMER1_COMPA_vect){  //leave this alone
+  static boolean state = true;
+  
+  TCNT1 = 0;
+  
+  if (state) {  //start pulse
+    digitalWrite(sigPin, onState);
+    OCR1A = PULSE_LENGTH * 2;
+    state = false;
+  } else{  //end pulse and calculate when to start the next pulse
+    static byte cur_chan_numb;
+    static unsigned int calc_rest;
+  
+    digitalWrite(sigPin, !onState);
+    state = true;
+
+    if(cur_chan_numb >= CHANNEL_NUMBER){
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PULSE_LENGTH;// 
+      OCR1A = (FRAME_LENGTH - calc_rest) * 2;
+      calc_rest = 0;
+    }
+    else{
+      OCR1A = (ppm[cur_chan_numb] - PULSE_LENGTH) * 2;
+      calc_rest = calc_rest + ppm[cur_chan_numb];
+      cur_chan_numb++;
+    }     
+  }
+}
